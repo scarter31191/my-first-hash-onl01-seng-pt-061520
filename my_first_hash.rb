8041d08ba3429f7c73a02c8729fe5d6b3807a2f2
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 05:11:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+  shipping_manifest[]# add 2 muskets to the shipping_manifest hash below
 
 
 
